2f2c0abe415396892a4a09515341e0bcf1bdb6c6
unknown <abhishekrajpoudel@gmail.com>
2022-07-08 12:28:51 +0545

    Learned how to use scroll trigger to animate object only on scroll and now will do the same for videos

--- Gsap-learning/app.js
+++ Gsap-learning/app.js
@@ -4,11 +4,27 @@ gsap.registerPlugin(ScrollTrigger);
 
 //a gsap animation to go from where you are to 700px to right and that
 //gets triggered when you scroll
-gsap.to(".square", {
-  x: 700,
-  duration: 3,
-  scrollTrigger: {
-    trigger: ".square",
-    start: "top center",
-  },
-});
+// gsap.to(".square", {
+//   x: 1000,
+//   duration: 15,
+//   scrollTrigger: {
+//     trigger: ".othersquare",
+//     start: "top center",
+//     end: "top 30%",
+//     //You can give it true of some number which will be
+//     //the amount of time it takes to smooth our animation
+//     scrub: 5,
+//     // toggleActions: onStart,onLeave,onEnterAgain,onLeaveAgain
+//     // toggleActions: "restart pause resume reset",
+//     pin: ".square",
+//     pinSpacing: true,
+//     // markers: true,
+//   },
+// });
+
+const timeline = gsap.timeline();
+
+timeline
+  .to(".timeline", { x: 500, duration: 3 })
+  .to(".timeline", { y: 500, duration: 1 })
+  .to(".timeline", { x: 0, y: 0, rotation: 360, duration: 1 });
